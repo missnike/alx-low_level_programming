72dc4f47af93cd9712b685988ef6a016fbb45973
missnike <adesinaadedamola4@gmail.com>
2023-09-07 21:09:36 +0000

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,13 @@ int main(void)
 	int i;
 	float f;
 	char c;
+	long g;
+	long long e;
 
 	printf("Size of a char: %lu(s).\n", (unsigned long)sizeof(c));
 	printf("Size of an int:%lu(s).\n", (unsigned long)sizeof(i));
-	printf("Size of a long int:%lu(s).\n", (unsigned long)sizeof(i));
-	printf("Size of a long long int:%lu(s).\n", (signed long)sizeof(i));
+	printf("Size of a long int:%lu(s).\n", (unsigned long)sizeof(g));
+	printf("Size of a long long int:%lu(s).\n", (unsigned long)sizeof(e));
 	printf("Size of a float:%lu(s).\n", (unsigned long)sizeof(f));
 
 	return (0);
