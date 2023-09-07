b7ec3d7003fe5b1a0e01cee74278110046b1b689
missnike <adesinaadedamola4@gmail.com>
2023-09-07 20:41:24 +0000

    New commit

---
+++ 0x00-hello_world/6-sizes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always return 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+	float f;
+	char c;
+
+	printf("Size of a char: %lu(s).\n", (unsigned long)sizeof(c));
+	printf("Size of an int:%lu(s).\n", (unsigned long)sizeof(i));
+	printf("Size of a long int:%lu(s).\n", (unsigned long)sizeof(i));
+	printf("Size of a long long int:%lu(s).\n", (signed long)sizeof(i));
+	printf("Size of a float:%lu(s).\n", (unsigned long)sizeof(f));
+
+	return (0);
+
+}
