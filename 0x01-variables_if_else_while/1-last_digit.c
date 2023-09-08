3384dc6621a26aac71d42c64ca021ea56866fc7a
missnike <adesinaadedamola4@gmail.com>
2023-09-08 22:55:08 +0000

    new commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - the main entry
+ *  Discription: checks on the last digit of the assigned var
+ *  Return: alway return 0 (success)
+ */
+int main(void)
+{
+
+	int n, r;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	r = n % 10;
+	if (r > 5)
+	{
+		printf("Last digit of %d and is greater than 5\n", n, r);
+	}
+	else if (r < 6 && r != 0)
+	{
+		printf("Last digit of %d and is less than 6 and not 0\n", n, r);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, r);
+	}
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the main entry
+ *
+ * Return: Always return Zero
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+
+}
