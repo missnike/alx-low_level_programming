1572048af0608dfd8eb48d1ed854f5343d01bbda
missnike <adesinaadedamola4@gmail.com>
2023-09-10 21:32:14 +0000

    new commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int	num1, num2, num3;
 
-		for (num1 = '0'; num1 < '9'; num1++)
+		for (num1 = '0'; num1 < '7'; num1++)
 		{
-			for (num2 = num1 + 1; num2 <= '9'; num2++)
+			for (num2 = num1 + 1; num2 <= '8'; num2++)
 			{
 				for (num3 = num1 + 2; num3 <= '9'; num3++)
 				{
