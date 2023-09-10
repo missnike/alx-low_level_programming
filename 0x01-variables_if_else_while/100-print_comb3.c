c90ee79c52d8cbdb920e9ac02684d478ce891117
missnike <adesinaadedamola4@gmail.com>
2023-09-10 21:03:31 +0000

    new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,15 +14,19 @@ int main(void)
 			{
 			if (num2 != num1)
 			{
+
 			putchar(num1);
 		putchar(num2);
 if (num1 == '8' && num2 == '9')
 continue;
+
 putchar(',');
 putchar(' ');
 			}
 			}
 		}
+
 	putchar('\n');
 	return (0);
+
 }
