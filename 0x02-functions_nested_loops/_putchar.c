d43b8c73fb01cd4ec52894c6e8796fe1ec54f835
missnike <adesinaadedamola4@gmail.com>
2023-09-12 19:47:50 +0000

    the _putchar and main.h prototype

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ *  main - _putchar is a custom function
+ *  to writed a program instead of using printf
+ *
+ *  @c: A c function to print the characters
+ *
+ *  Return: Is to return the return assigned to it
+ */
+int _putchar(char c);
+{
+	return (write(1, &c, 1));
+}
