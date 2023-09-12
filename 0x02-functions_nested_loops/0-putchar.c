0425395765fe6fb057be98163391695621d51b41
missnike <adesinaadedamola4@gmail.com>
2023-09-12 06:53:49 +0000

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main entry
+ *
+ * Return: Always return zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX Funtions, nested loops n What is function prototypenScope of variables n Differencees between definations and declaration
