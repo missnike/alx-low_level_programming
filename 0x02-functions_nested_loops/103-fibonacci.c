12727f57921f0e30ef9f6fd9a79dc2d0606169e5
missnike <adesinaadedamola4@gmail.com>
2023-09-13 00:51:13 +0000

    new commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i
+	unsigned log int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+	if (j < 4000000 && (j % 2) == 0)
+	{
+	sum = sum + j;
+	}
+	next = j + k;
+	k = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
