5fd29c4dffad914626fc06c922a5c98567a79179
missnike <adesinaadedamola4@gmail.com>
2023-09-13 00:08:24 +0000

    PRINT ALPHABET and numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,7 @@ int print_last_digit(int a)
 	last_digit = a % 10;
 	if (last_digit < 0)
 	{
-	last_digit = last_digit * 1;
+	last_digit = last_digit * -1;
 	}
 	_putchar(last_digit + '0');
 	return (last_digit);
