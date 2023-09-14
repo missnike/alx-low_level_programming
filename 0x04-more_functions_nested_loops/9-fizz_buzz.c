06f147d282b8a8eb958196182fdf6bbeaa7b6fbf
missnike <adesinaadedamola4@gmail.com>
2023-09-14 16:26:36 +0000

    more on functions and nested loop

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,14 @@ nclude "main.h"
 int main(void)
 {
 		int i;
+
 		for (i = 1; i <= 100; i++)
 		{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 		printf("Buzz");
 		}
-		else if (i % 3 === 0 && i % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 		printf("FizzBuzz");
 		}
