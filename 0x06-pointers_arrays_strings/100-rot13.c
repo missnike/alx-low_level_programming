db7c00b5523ffd9ee46a9d98e80648cb1b2a78b3
missnike <adesinaadedamola4@gmail.com>
2023-09-21 21:03:53 +0000

    explicitly more on pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * rot13 - encodes a string in rot13
@@ -10,16 +10,16 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char x[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char y[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; x[j] != '\0'; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == x[j])
+			if (s[i] == a[j])
 			{
-				s[i] = y[j];
+				s[i] = b[j];
 				break;
 			}
 		}
