b9d6c37a5796977e26b67b017bb63740a86b91a1
missnike <adesinaadedamola4@gmail.com>
2023-10-03 00:22:11 +0000

    new commit

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,11 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
- * _putchar - the main function to print a character
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * return: return the inputed character
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
