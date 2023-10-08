764fb594b5fd5e531647441fc6baa0622d07f0ca
missnike <adesinaadedamola4@gmail.com>
2023-10-08 18:46:54 +0000

    creating calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,3 +17,26 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+/**
+ *_calloc - to allocates memory for an array
+ * @nmemb: the number of elements in the array
+ * @size: the size of each element
+ *
+ * Return: the pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
