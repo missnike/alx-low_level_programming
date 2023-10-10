32cc648eefb6922f5cfb8e9c6f62b4743b116a22
missnike <adesinaadedamola4@gmail.com>
2023-10-10 22:14:11 +0000

    love for structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,18 +2,19 @@
 /**
  * init_dog - to initialize the variables of struct dog
  *
- * *struct dog: the properties of dog
- * *name: the name of dog
- * age: the age of dog
- * *owner: the ownner of dog
+ * @*struct dog: the properties of dog
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the ownner of dog
+ * @d: a pointer to the structure of dog
  * Return: does not return anything
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-	d-> name = name;
-	d-> age = age;
-	d-> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 	}
 }
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+/**
+ * print_dog - to print null if d is empty
+ * @d: the pointer to struct dog
+ *
+ * Return: it will be empty
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
