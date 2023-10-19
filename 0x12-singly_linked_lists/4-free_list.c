ddf80a362ad33a8641ebb076fa1737d484a692d1
missnike <adesinaadedamola4@gmail.com>
2023-10-19 22:31:05 +0000

    the hare and the tortise

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - to frees a linked list
+ * @head: to list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
