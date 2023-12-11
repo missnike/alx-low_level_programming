e28affa7703b8648554a650cc7b843a554e10abe
missnike <adesinaadedamola4@gmail.com>
2023-12-11 14:28:16 -0800

    this girl wont kill me

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -20,6 +20,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(const char *haystack, const char *needle);
+
 
 #endif
